fafb55f92facc5d7ff6908f32125211d769ab5fe
Felippe Vaz <felippe.vz@gmail.com>
2026-03-07 16:00:59 -0300

    feat: add RegexPath

--- src/main/java/dev/felippevaz/router/Route.java
+++ src/main/java/dev/felippevaz/router/Route.java
@@ -5,13 +5,15 @@ import java.lang.reflect.Method;
 public class Route {
 
     private final String method;
+    private final String regexPath;
     private final String path;
     private final Object controller;
     private final Method handler;
 
 
-    public Route(String method, String path, Object controller, Method handler) {
+    public Route(String method, String regexPath, String path, Object controller, Method handler) {
         this.method = method;
+        this.regexPath = regexPath;
         this.path = path;
         this.controller = controller;
         this.handler = handler;
@@ -31,5 +33,9 @@ public class Route {
 
     public Method getHandler() {
         return handler;
+    }
+
+    public String getRegexPath() {
+        return this.regexPath;
     }
 }
