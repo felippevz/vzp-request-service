5dc97d24ac3aab6a0eb8c520883965345c4081bc
Felippe Vaz <felippe.vz@gmail.com>
2026-03-07 16:00:47 -0300

    feat: add ToDo

--- src/main/java/dev/felippevaz/server/RequestServer.java
+++ src/main/java/dev/felippevaz/server/RequestServer.java
@@ -35,9 +35,7 @@ public class RequestServer {
 
         } catch (IOException exception) {
 
-
-            exception.printStackTrace();
-            //treat error later
+            //todo: treat error later
         }
     }
 }
