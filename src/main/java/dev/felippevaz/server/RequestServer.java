e66b261e8fec6ced1461bc3c400441e13fb285ca
Felippe Vaz <felippe.vz@gmail.com>
2026-03-11 14:39:46 -0300

    refactor: delegate SSLContext responsibility to the user for better security

--- src/main/java/dev/felippevaz/server/RequestServer.java
+++ src/main/java/dev/felippevaz/server/RequestServer.java
@@ -20,7 +20,6 @@ public class RequestServer {
     private final RequestHandler requestHandler;
     private Executor executor;
     private int backLog;
-    private SSLContext sslContext;
     private HttpsConfigurator configurator;
 
     public RequestServer(int port) {
@@ -28,8 +27,6 @@ public class RequestServer {
         this.backLog = 0;
         this.requestHandler = new RequestHandler(new Router());
         this.executor = Executors.newFixedThreadPool(1);
-        this.sslContext = getDefaultSSLContext();
-        this.configurator = new HttpsConfigurator(this.sslContext);
     }
 
     public void registerController(Object controller) {
@@ -59,10 +56,6 @@ public class RequestServer {
         this.configurator = configurator;
     }
 
-    public void setSSLContext(SSLContext sslContext) {
-        this.sslContext = sslContext;
-    }
-
     public void setBackLog(int backLog) {
         this.backLog = backLog;
     }
@@ -73,33 +66,5 @@ public class RequestServer {
 
     public int getPort() {
         return this.port;
-    }
-
-    private SSLContext getDefaultSSLContext() {
-
-        SSLContext context;
-
-        try {
-
-            char[] password = "password".toCharArray();
-
-            KeyStore keyStore = KeyStore.getInstance("JKS");
-            keyStore.load(
-                    getClass().getResourceAsStream("/keys/test-keystore.jks"),
-                    password
-            );
-
-            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-
-            keyManagerFactory.init(keyStore, password);
-
-            context = SSLContext.getInstance("TLS");
-            context.init(keyManagerFactory.getKeyManagers(), null, new SecureRandom());
-
-            return context;
-
-        } catch (Exception e){
-            throw new RuntimeException("error in certificate");
-        }
     }
 }
